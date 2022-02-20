de843f9d6c9d6a1b89cb0c5a4aad1ce0b2c0c56f
Lucas Bueno <lucas-jbb@hotmail.com>
2022-02-20 20:14:23 -0300

    fix: eslint rules

--- src/presentation/pages/MoviesList/index..tsx
+++ src/presentation/pages/MoviesList/index..tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect, useCallback } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 import InfiniteScroll from 'react-infinite-scroll-component'
